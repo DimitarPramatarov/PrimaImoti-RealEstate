e183fe9c2a0e647c5c8f9cf7358ba75c829cd5cb
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-13 21:37:23 +0200

    Update class

--- src/PrimaImoti.DataModels/Contact/Person.cs
+++ src/PrimaImoti.DataModels/Contact/Person.cs
@@ -9,13 +9,14 @@
             this.LastName = lastName;
             this.Email = email;
         }
-    
-        public int Id {get; set;}
 
-        public string FirstName {get; set;}
+        public int Id { get; set; }
 
-        public string LastName {get; set;}
+        public string FirstName { get; set; }
 
-        public string Email {get; set;}
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
     }
 }
