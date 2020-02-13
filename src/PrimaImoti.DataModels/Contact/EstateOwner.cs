0721683f8a9d535aa4ea59200ce4d0ceaf3652c3
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-13 21:36:23 +0200

    Added EstateOwner

---
+++ src/PrimaImoti.DataModels/Contact/EstateOwner.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimaImoti.DataModels.Contact
+{
+    public class EstateOwner : Person
+    {
+        public EstateOwner(string firstName, string lastName, string email, string phone)
+            : base(firstName, lastName, email)
+        {
+            this.Phone = phone;
+        }
+
+
+        public string Phone { get; set; }
+    }
+}
