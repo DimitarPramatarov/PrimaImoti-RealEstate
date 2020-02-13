303e7bfc65d1cf2881b3ea04bfba065ae42e44e4
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-13 21:07:40 +0200

    some dlls

--- src/PrimaImoti.ViewModels/ViewModels/WaitingEstatesViewModel.cs
+++ src/PrimaImoti.ViewModels/ViewModels/WaitingEstatesViewModel.cs
@@ -5,90 +5,26 @@ namespace PrimaImoti.ViewModels.ViewModels
 {
     public class WaitingEstatesViewModel
     {
-        public Person Person {get; set;}
+        [Display(Name = "Статус")]
+        public bool Aproved {get; set;}
+
+        [Display(Name = "Име")]
+
+        public string FirstName {get; set;}
+
+        [Display(Name = "Фамилия")]
+        public string LastName {get; set;}
+
+        [Display(Name = "Телефон")]
+        public string Phone {get; set;}
+
+        [Display(Name = "Тип имот")]
+        public PropertyType Type {get; set;}
+
+        [Display(Name = "Адрес")]
+        public string Adress {get; set;}
 
         [Display(Name = "Дата")]
         public string Date {get; set;}
-
-        [Display(Name = "Тип Имот")]
-        public PropertyType Type { get; set; }
-
-        [Display(Name = "Цена")]
-        public double Price { get; set; }
-
-        public string Currency {get; set;}
-
-        [Display(Name = "Описание на имота")]
-        public string Description { get; set; }
-
-        [Display(Name = "Локация")]
-        public string Location { get; set; }
-
-        
-        [Display(Name = "Точен Адрес")]
-        public string Adress { get; set; }
-
-        [Display(Name = "Под район")]
-        public string SecondLocation {get; set;}
-
-        [Display(Name = "Квадратура")]
-        public int Area { get; set; }
-
-        [Display(Name = "Обзавеждане")]
-        public string Furniture { get; set; }
-
-        [Display(Name = "Строителство")]
-        public string Building { get; set; }
-
-        [Display(Name = "Етаж")]
-        public int Floor { get; set; }
-
-        [Display(Name = "Отопление")]
-        public string Heating { get; set; }
-
-        [Display(Name = "С гараж")]
-        public bool Garage {get; set; }
-
-        [Display(Name = "С преход")]
-        public bool WithTransition {get; set;} = false;
-
-        [Display(Name = "С асансьор")]
-        public bool WithElevator {get; set;} = false;
-
-        [Display(Name = "В строеж")]
-        public bool InBuildingProcess {get; set;} = false;
-
-         [Display(Name = "Лизинг")]
-        public bool Credit {get; set;} = false;
-
-        [Display(Name = "Ипотекиран")]
-        public bool iSMortgage {get; set;} = false;
-
-        [Display(Name = "Паркинг")]
-        public bool Parking {get; set;} = false;
-
-        [Display(Name = "Бартер")]
-        public bool Trade {get; set;} = false;
-
-        [Display(Name = "Интернет връзка")]
-        public bool WithInternetConnection {get; set; } = false;
-
-        [Display(Name = "Видео наблюдение")]
-        public bool VideoSecurity {get; set;} = false;
-
-        [Display(Name = "Контрол на достъп")]
-        public bool AccsessControl {get; set;} = false;
-
-        [Display(Name = "Охрана")]
-        public bool Security {get; set;} = false;
-
-        [Display(Name = "Саниран")]
-        public bool Renovated {get; set;}
-
-        [Display(Name = "С действащ бизнес")]
-        public bool WithBussiness {get; set;}
-
-        [Display(Name = "Снимка")]
-        public byte[] FormFile { get; set; }
     }
 }
--- src/PrimaImoti/Areas/Administration/Controllers/AdminController.cs
+++ src/PrimaImoti/Areas/Administration/Controllers/AdminController.cs
@@ -46,30 +46,12 @@ namespace PrimaImoti.Areas.Administration.Controllers
         public IActionResult WaitingEstates()
         {
             List<WaitingEstatesViewModel> waitingEstates = _context.Ads
-                .Where(estate => estate.Aproved == true)
+                .Where(estate => estate.Aproved == false)
                 .Select(estateFromDb => new WaitingEstatesViewModel
                 {
                     Date = estateFromDb.CreatedOn.ToString(),
-                    Person = estateFromDb.Person,
                     Type = estateFromDb.Type,
-                    Price = estateFromDb.Price,
-                    Currency = estateFromDb.Curency,
-                    Description = estateFromDb.Description,
-                    Location = estateFromDb.Estate.Location,
                     Adress = estateFromDb.Estate.Adress,
-                    SecondLocation = estateFromDb.Estate.SecondLocation,
-                    Area = estateFromDb.Estate.Area,
-                    Furniture = estateFromDb.Estate.Furniture,
-                    Building = estateFromDb.Estate.Building,
-                    Floor = estateFromDb.Estate.Floor,
-                    Heating = estateFromDb.Estate.Heating,
-                    Garage = estateFromDb.Estate.EstateFeatures.Garage,
-                    WithTransition= estateFromDb.Estate.EstateFeatures.Transition,
-                    WithElevator= estateFromDb.Estate.EstateFeatures.Elevator,
-                    InBuildingProcess= estateFromDb.Estate.EstateFeatures.InBuilding,
-                    Credit = estateFromDb.Estate.EstateFeatures.Credit,
-                    iSMortgage = estateFromDb.Estate.EstateFeatures.Martgage,
-                    
                 }).ToList();
 
 
