0369c436ed6ad98a8e95dff6ec45f0c7ab443374
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-14 09:42:24 +0200

    Contact service
    Send message from form to admin

---
+++ src/PrimaImoti.Services.Data/Messages/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using PrimaImoti.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PrimaImoti.Services.Data
+{
+    public interface IContactService
+    {
+        Task SendMessageAsync(string firstName, string lastName, string email, string content, string subject);
+
+        Task<IEnumerable<ContactViewModel>> AllMessages(int page = 1);
+    }
+}
