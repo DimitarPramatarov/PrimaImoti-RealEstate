76a0c9d0837c04b95e354a3ab61a2b8e4faa090a
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-14 09:43:11 +0200

    DbContext is separated

---
+++ src/PrimaImoti.Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PrimaImoti.DataModels;
+using PrimaImoti.DataModels.Ad;
+using PrimaImoti.DataModels.Contact;
+using PrimaImoti.DataModels.Estate;
+
+namespace PrimaImoti.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<PrimaImotiUser, IdentityRole, string>
+    {
+        public DbSet<EstateProperty> Estates { get; set; }
+
+        public DbSet<Ad> Ads { get; set; }
+
+
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<Contact> Contacts {get; set;}
+
+        public DbSet<EstateFeatures> EsteateFeatures {get; set;} 
+       
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
