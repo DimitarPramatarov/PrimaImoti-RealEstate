c21cab1ad0ac006816fd8cf57e9d42ee0823004b
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-14 09:43:30 +0200

    Added constructor

--- src/PrimaImoti.DataModels/Contact/Contact.cs
+++ src/PrimaImoti.DataModels/Contact/Contact.cs
@@ -6,6 +6,18 @@ namespace PrimaImoti.DataModels.Contact
 {
     public class Contact
     {
+
+        public Contact(DateTime created, Person sender, Message message)
+        {
+            this.Created = created;
+            this.Sender = sender;
+            this.Message = message;
+        }
+
+        public Contact()
+        {
+        }
+
         public int Id {get; set;}
 
         public DateTime Created {get; set;}
