226c51d1ec646ca1c341f596f0e1bc8042452056
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-16 13:09:41 +0200

    Fix the model

--- src/PrimaImoti.Services.Data/Messages/Models/MessageDetailsServiceModel.cs
+++ src/PrimaImoti.Services.Data/Messages/Models/MessageDetailsServiceModel.cs
@@ -4,7 +4,7 @@ using PrimaImoti.Services.Mappings;
 
 namespace PrimaImoti.Services.Data.Messages.Models
 {
-    public class MessageDetailsServiceModel : IMapFrom<Message>, IHaveCustomMappings
+    public class MessageDetailsServiceModel : IMapFrom<Message>
     {
         public string Id { get; set; }
 
@@ -20,20 +20,6 @@ namespace PrimaImoti.Services.Data.Messages.Models
 
         public string Phone { get; set; }
 
-        public string Created { get; set; }
-
-
-        public void CreateMappings(IProfileExpression configuration)
-        {
-            configuration.CreateMap<Message, MessageDetailsServiceModel>()
-                  .ForMember(a => a.FirstName, opt => opt.MapFrom(x => x.Sender.FirstName))
-                  .ForMember(a => a.LastName, opt => opt.MapFrom(x => x.Sender.LastName))
-                  .ForMember(a => a.Email, opt => opt.MapFrom(x => x.Sender.Email))
-                  .ForMember(a => a.Phone, opt => opt.MapFrom(x => x.Sender.Phone))
-                  .ForMember(a => a.Phone, opt => opt.MapFrom(x => x.Sender.Id));
-        }
-        //   => configuration
-        //   .CreateMap<Message, MessageDetailsServiceModel>()
-
+        public string CreatedOn { get; set; }
     }
 }
