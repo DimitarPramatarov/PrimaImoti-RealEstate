1c2ae8a490c36ee96fa130b360548521e3fc0790
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2020-02-16 13:06:34 +0200

    Added Mapping Profile
    Here will maintain all profiles

---
+++ src/PrimaImoti.Services/Mappings/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PrimaImoti.DataModels;
+using PrimaImoti.Services.Data.Messages.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimaImoti.Services.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Message, MessageDetailsServiceModel>()
+                .ForMember(a => a.FirstName, cfg => cfg.MapFrom(b => b.Sender.FirstName))
+                .ForMember(a => a.LastName, cfg => cfg.MapFrom(b => b.Sender.LastName))
+                .ForMember(a => a.Phone, cfg => cfg.MapFrom(b => b.Sender.Phone))
+                .ForMember(a => a.Email, cfg => cfg.MapFrom(b => b.Sender.Email))
+                .ForMember(a => a.CreatedOn, cfg => cfg.MapFrom(b => b.Created));
+        }
+    }
+}
